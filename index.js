2dd11b118b0235aa8129eee754701ec23c33f8f0
keepkilay <alekseevvobis@mail.ru>
2023-09-08 15:21:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+celsius = prompt('Введите количество градусов по Цельсию')
+fahrenheit = celsius * 9 / 5 + 32;
+alert(celsius + ' градусов по Цельсию равны ' + fahrenheit + ' градусам по Фаренгейту')
+
